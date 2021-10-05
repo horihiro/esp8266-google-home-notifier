33cefaf8abfeed8c60f6857944fa24c4b02c6394
horihiro <hori.kawa.shima.tool+github@gmail.com>
2021-10-05 09:12:44 +0900

    change return value

--- src/ESP8266mDNS.cpp
+++ src/ESP8266mDNS.cpp
@@ -566,7 +566,7 @@ MDNSTxt * MDNSResponder::_getServiceTxt(char *name, char *proto){
   MDNSService* servicePtr;
   for (servicePtr = _services; servicePtr; servicePtr = servicePtr->_next) {
     if(servicePtr->_port > 0 && strcmp(servicePtr->_name, name) == 0 && strcmp(servicePtr->_proto, proto) == 0){
-      if (servicePtr->_txts == 0) return false;
+      if (servicePtr->_txts == 0) return 0;
       else{
         return servicePtr->_txts;
       }
@@ -579,7 +579,7 @@ uint16_t MDNSResponder::_getServiceTxtLen(char *name, char *proto){
   MDNSService* servicePtr;
   for (servicePtr = _services; servicePtr; servicePtr = servicePtr->_next) {
     if(servicePtr->_port > 0 && strcmp(servicePtr->_name, name) == 0 && strcmp(servicePtr->_proto, proto) == 0){
-      if (servicePtr->_txts == 0) return false;
+      if (servicePtr->_txts == 0) return 0;
       else{
         return servicePtr->_txtLen;
       }
